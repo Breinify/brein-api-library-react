58dc4cdac74332eb6587d1e7fe40087100b5e37c
Alvin <alvindkwong@gmail.com>
2022-07-12 11:11:13 -0700

    - Format `setup` - Export `setup` in `index`

--- src/setup/setup.ts
+++ src/setup/setup.ts
@@ -1,8 +1,8 @@
-const BreinifyGlobalConfigs = {} as { apiKey?: string, secret?: string };
+const BreinifyGlobalConfigs = {} as { apiKey?: string; secret?: string };
 
 export function BreinifySetup(apiKey: string, secret: string) {
-  BreinifyGlobalConfigs.apiKey = apiKey;
-  BreinifyGlobalConfigs.secret = secret;
+	BreinifyGlobalConfigs.apiKey = apiKey;
+	BreinifyGlobalConfigs.secret = secret;
 }
 
 export default BreinifyGlobalConfigs;
