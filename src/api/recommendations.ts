1ae0c3a722238d29a90b99fe6b260c71e5c26194
Alvin <alvindkwong@gmail.com>
2022-07-13 13:45:01 -0700

    - Use relative path

--- src/api/recommendations.ts
+++ src/api/recommendations.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { API, RECOMMENDATION } from '../configs';
-import { BreinifyGlobalConfigs } from 'src/setup';
+import { BreinifyGlobalConfigs } from '../setup';
 
 const URL = `${API}/${RECOMMENDATION}`;
 
