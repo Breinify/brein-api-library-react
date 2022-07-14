c2d84974cb4a4137fbc7af0951b4ecc59e89b85a
Alvin <alvindkwong@gmail.com>
2022-07-14 15:30:42 -0700

    Make apiKey required for `BreinifySetup`

--- src/setup/setup.ts
+++ src/setup/setup.ts
@@ -1,17 +1,21 @@
-type ConfigProps = {
+interface IConfig {
 	apiKey?: string;
 	secret?: string;
-};
+}
 
-export const BreinifyGlobalConfigs = {} as ConfigProps;
+type RequiredProperties = keyof typeof requiredSetup;
+type RequiredPickConfig = Required<Pick<IConfig, RequiredProperties>>;
+type SetupProps = IConfig & RequiredPickConfig;
 
 // Required keys needed to initialize BreinifyGlobalConfigs
 export const requiredSetup = {
 	apiKey: 'apiKey',
 };
 
+export const BreinifyGlobalConfigs = {} as IConfig;
+
 // Users call this on their app (in the highest level)
-export function BreinifySetup({ apiKey, secret }: ConfigProps): void {
+export function BreinifySetup({ apiKey, secret }: SetupProps): void {
 	BreinifyGlobalConfigs.apiKey = apiKey;
 	BreinifyGlobalConfigs.secret = secret;
 }
