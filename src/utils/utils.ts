4d8d9e76ea34a519d505c414760c24b60da2b667
Alvin <alvindkwong@gmail.com>
2022-07-19 17:14:03 -0700

    Remove logs

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,5 @@ import { BreinifyGlobalConfigs, requiredSetup } from '../setup';
 
 // Checks if BreinifySetup is complete
 export function isSetupComplete() {
-	console.log('BreinifyGlobalConfigs: ', BreinifyGlobalConfigs);
 	return Object.keys(requiredSetup).every((value) => value in BreinifyGlobalConfigs);
 }
