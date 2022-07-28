a50112fe3aa6520550fbdc30f4dc57814e2e6924
Alvin <alvindkwong@gmail.com>
2022-07-27 18:02:24 -0700

    Update useLoader test structure and naming

--- src/hooks/helpers/useLoader.test.ts
+++ src/hooks/helpers/useLoader.test.ts
@@ -3,117 +3,172 @@ import { useLoader } from './useLoader';
 import { STATUS } from '../../types';
 
 describe('useLoader', () => {
-	test('init state when calling useLoader', () => {
-		const defaultStatus = STATUS.REQUESTING;
-		const defaultDataState = {};
-		const { result } = renderHook(() => useLoader(defaultStatus, defaultDataState));
+	describe('initialization', () => {
+		test('loadingStatus should be INIT', () => {
+			const defaultStatus = STATUS.INIT;
+			const { result } = renderHook(() => useLoader());
 
-		expect(result.current.data).toEqual(defaultDataState);
-		expect(result.current.loadingStatus.status).toEqual(defaultStatus);
+			expect(result.current.loadingStatus.status).toEqual(defaultStatus);
+		});
+
+		test('data should be null', () => {
+			const defaultDataState = null;
+			const { result } = renderHook(() => useLoader());
+
+			expect(result.current.data).toEqual(defaultDataState);
+		});
+
+		test('passing defaultStatus should change the default loadingStatus', () => {
+			const defaultStatus = STATUS.REQUESTING;
+			const { result } = renderHook(() => useLoader(defaultStatus));
+
+			expect(result.current.loadingStatus.status).toEqual(defaultStatus);
+		});
+
+		test('passing defaultDataState should change the default data', () => {
+			const defaultDataState = {};
+			const { result } = renderHook(() => useLoader(undefined, defaultDataState));
+
+			expect(result.current.data).toEqual(defaultDataState);
+		});
 	});
 
-	test('after calling setSuccess, setLoading should preserve data and change the loadingStatus.status to REQUESTING', () => {
-		const { result } = renderHook(() => useLoader());
-		const response = 'response';
-		act(() => {
-			result.current.setSuccess(response);
-		});
-		act(() => {
-			result.current.setLoading();
+	describe('setLoading', () => {
+		test('should set loadingStatus to REQUESTING', () => {
+			const { result } = renderHook(() => useLoader());
+			act(() => {
+				result.current.setLoading();
+			});
+
+			expect(result.current.loadingStatus.status).toEqual(STATUS.REQUESTING);
 		});
 
-		expect(result.current.loadingStatus.status).toEqual(STATUS.REQUESTING);
-		expect(result.current.data).toEqual(response);
+		test('after success, setLoading with nothing passed should preserve data', () => {
+			const { result } = renderHook(() => useLoader());
+			const response = 'response';
+			act(() => {
+				result.current.setSuccess(response);
+			});
+			act(() => {
+				result.current.setLoading();
+			});
+
+			expect(result.current.data).toEqual(response);
+		});
+
+		test('isRefetch: true should change loadingStatus to REFETCH', () => {
+			const { result } = renderHook(() => useLoader());
+			act(() => {
+				result.current.setLoading(true);
+			});
+
+			expect(result.current.loadingStatus.status).toEqual(STATUS.REFETCH);
+		});
+
+		test('after success, setLoading with shouldClearData: true should clear data', () => {
+			const { result } = renderHook(() => useLoader());
+			const response = 'response';
+			act(() => {
+				result.current.setSuccess(response);
+			});
+			act(() => {
+				result.current.setLoading(false, true);
+			});
+
+			expect(result.current.data).toEqual(null);
+		});
 	});
 
-	test('setLoading with isRefetch: true should change loadingStatus.status to REFETCH', () => {
-		const { result } = renderHook(() => useLoader());
-		act(() => {
-			result.current.setLoading(true);
+	describe('setSuccess', () => {
+		test('should set loadingStatus to SUCCESS', () => {
+			const { result } = renderHook(() => useLoader());
+			act(() => {
+				result.current.setSuccess();
+			});
+
+			expect(result.current.loadingStatus.status).toEqual(STATUS.SUCCESS);
 		});
 
-		expect(result.current.loadingStatus.status).toEqual(STATUS.REFETCH);
+		test('passing the response should set data with the response', () => {
+			const { result } = renderHook(() => useLoader());
+			const response = 'response';
+			act(() => {
+				result.current.setSuccess(response);
+			});
+
+			expect(result.current.data).toEqual(response);
+		});
+
+		test('passing no response should set data to null', () => {
+			const { result } = renderHook(() => useLoader());
+			act(() => {
+				result.current.setSuccess();
+			});
+
+			expect(result.current.loadingStatus.status).toEqual(STATUS.SUCCESS);
+			expect(result.current.data).toEqual(null);
+		});
 	});
 
-	test('after calling setSuccess, setLoading with shouldClearData: true should clear data and change the loadingStatus.status to REQUESTING', () => {
-		const { result } = renderHook(() => useLoader());
-		const response = 'response';
-		act(() => {
-			result.current.setSuccess(response);
-		});
-		act(() => {
-			result.current.setLoading(false, true);
-		});
+	describe('onError', () => {
+		test('should set loadingStatus to FAILURE', () => {
+			const { result } = renderHook(() => useLoader());
+			act(() => {
+				result.current.setError();
+			});
 
-		expect(result.current.loadingStatus.status).toEqual(STATUS.REQUESTING);
-		expect(result.current.data).toEqual(null);
-	});
-
-	test('passing the response into setSuccess should set data with the response', () => {
-		const { result } = renderHook(() => useLoader());
-		const response = 'response';
-		act(() => {
-			result.current.setSuccess(response);
+			expect(result.current.loadingStatus.status).toEqual(STATUS.FAILURE);
 		});
 
-		expect(result.current.loadingStatus.status).toEqual(STATUS.SUCCESS);
-		expect(result.current.data).toEqual(response);
-	});
+		test('after success, passing error should set error', () => {
+			const { result } = renderHook(() => useLoader());
+			const error = 'error';
+			act(() => {
+				result.current.setError(error);
+			});
 
-	test('passing no response into setSuccess should set data to null', () => {
-		const { result } = renderHook(() => useLoader());
-		act(() => {
-			result.current.setSuccess();
+			expect(result.current.loadingStatus.reason).toEqual(error);
 		});
 
-		expect(result.current.loadingStatus.status).toEqual(STATUS.SUCCESS);
-		expect(result.current.data).toEqual(null);
-	});
+		test('after success, error should preserve data', () => {
+			const { result } = renderHook(() => useLoader());
+			const response = 'response';
+			const error = 'error';
+			act(() => {
+				result.current.setSuccess(response);
+			});
+			act(() => {
+				result.current.setError(error);
+			});
 
-	test('after setting response to setSuccess, setError with an error should change loadingStatus.status to FAILURE, set error with the error, and preserve data', () => {
-		const { result } = renderHook(() => useLoader());
-		const response = 'response';
-		const error = 'error';
-		act(() => {
-			result.current.setSuccess(response);
-		});
-		act(() => {
-			result.current.setError(error);
-		});
-
-		expect(result.current.loadingStatus.status).toEqual(STATUS.FAILURE);
-		expect(result.current.loadingStatus.reason).toEqual(error);
-		expect(result.current.data).toEqual(response);
-	});
-
-	test("after setting response to setSuccess, setError with NO error should change loadingStatus.status to FAILURE, set error to '', and preserve data", () => {
-		const { result } = renderHook(() => useLoader());
-		const response = 'response';
-		act(() => {
-			result.current.setSuccess(response);
-		});
-		act(() => {
-			result.current.setError();
+			expect(result.current.data).toEqual(response);
 		});
 
-		expect(result.current.loadingStatus.status).toEqual(STATUS.FAILURE);
-		expect(result.current.loadingStatus.reason).toEqual('');
-		expect(result.current.data).toEqual(response);
-	});
+		test("after success, passing no error should set error to ''", () => {
+			const { result } = renderHook(() => useLoader());
+			const response = 'response';
+			act(() => {
+				result.current.setSuccess(response);
+			});
+			act(() => {
+				result.current.setError();
+			});
 
-	test('after setting response to setSuccess, setError with an error and shouldClearData: true should change loadingStatus.status to FAILURE, set error with the error, and clear data', () => {
-		const { result } = renderHook(() => useLoader());
-		const response = 'hello';
-		const error = 'error';
-		act(() => {
-			result.current.setSuccess(response);
-		});
-		act(() => {
-			result.current.setError(error, true);
+			expect(result.current.loadingStatus.reason).toEqual('');
 		});
 
-		expect(result.current.loadingStatus.status).toEqual(STATUS.FAILURE);
-		expect(result.current.loadingStatus.reason).toEqual(error);
-		expect(result.current.data).toEqual(null);
+		test('after success, shouldClearData: true should clear data', () => {
+			const { result } = renderHook(() => useLoader());
+			const response = 'hello';
+			const error = 'error';
+			act(() => {
+				result.current.setSuccess(response);
+			});
+			act(() => {
+				result.current.setError(error, true);
+			});
+
+			expect(result.current.data).toEqual(null);
+		});
 	});
 });
