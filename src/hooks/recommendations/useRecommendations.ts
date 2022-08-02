86f6270540d1b26d08b97c4ead087119c964f509
Timothy Yue <62864298+timothyyue-b@users.noreply.github.com>
2022-08-02 13:25:58 -0700

    Revert "Merge branch 'main' of https://github.com/Breinify/brein-api-library-react"
    This reverts commit 5a343b1def5ce8726ecbb773d95c6cdb429b7b49, reversing
    changes made to a4474637bb9003768201f7e46a0a1c77dd353d8d.

--- src/hooks/recommendations/useRecommendations.ts
+++ src/hooks/recommendations/useRecommendations.ts
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 
 // HOOKS
-import { useLoader } from '../helpers';
+import { useLoader } from '../../hooks/helpers';
 
 // TYPES
 import { RecommendationQuery, STATUS } from '../../types';
@@ -27,7 +27,6 @@ export const useRecommendations = <T = any>(defaultDataState: any = null) => {
 	return {
 		getRecs,
 		data,
-		isInit: loadingStatus.status === STATUS.INIT,
 		isLoading: loadingStatus.status === STATUS.REQUESTING,
 		isSuccess: loadingStatus.status === STATUS.SUCCESS,
 		isFailure: loadingStatus.status === STATUS.FAILURE,
